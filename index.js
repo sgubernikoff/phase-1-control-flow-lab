2fe3a54d495c814475f529c183858b3289ad1b75
Sam Gubernikoff <sgubernikoff@gmail.com>
2022-01-30 17:40:13 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let ride = 0;
+function scuberGreetingForFeet(ride, distance) {
+  if (ride <= 400) return "This one is on me!";
+  else if (ride > 2000 && ride < 2500)
+    return "I will gladly take your thirty bucks.";
+  else if (ride > 2500) return "No can do.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
